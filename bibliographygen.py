1957b7c4e1f2598867637682c19c663311f5b5e0
David Reeves <djreeves2005@gmail.com>
2018-02-28 16:07:52 -0500

    Bibliography Generator Update: Untested
    Warning: This version has not been tested

--- bibliographygen.py
+++ bibliographygen.py
@@ -1,6 +1,7 @@
 print ("[Bibliography Generator]")
 print ("Current MLA: MLA8")
 print ("\nPress enter to continue...")
+retry = False
 authfirsttrue = False
 authlasttrue = False
 webtitletrue = False
@@ -13,13 +14,26 @@ article = input ("Please enter the article title (Not required)\n> ")
 while authfirsttrue == False:
   authfirst = input ("Please enter the author's first name (Include no spaces)\n> ")
   authfirsttrue = True
-authlast = input ("Please enter the author's last name (Include no spaces)\n> ")
-webtitle = input ("Please enter the website title\n> ")
-sponser = input ("Please enter the publisher/sponser (Not required)\n> ")
-url = input ("Please enter the URL of the website\n> ")
-monthpub = input ("What month was this published (Enter a number 1-12)\n> ")
-daypub = input ("What year was this published (Enter a number 1-31)\n> ")
-yearpub = input ("What day was this published (Enter a number [EX: 2018])\n> ")
+while authlasttrue == False:
+  authlast = input ("Please enter the author's last name (Include no spaces)\n> ")
+  authlasttrue = True
+while webtitletrue == False:
+  webtitle = input ("Please enter the website title\n> ")
+  webtitletrue = True
+if retry == False:
+  sponser = input ("Please enter the publisher/sponser (Not required)\n> ")
+while urltrue == False:
+  url = input ("Please enter the URL of the website\n> ")
+  urltrue = True
+while monthtrue == False:
+  monthpub = input ("What month was this published (Enter a number 1-12)\n> ")
+  monthtrue = True
+while daytrue == False:
+  daypub = input ("What year was this published (Enter a number 1-31)\n> ")
+  daytrue = True
+while yeartrue == False:
+  yearpub = input ("What day was this published (Enter a number [EX: 2018])\n> ")
+  yeartrue = True
 if authfirst == "":
   print ("Author's first name has no value!")
   authfirsttrue = False
@@ -57,20 +71,18 @@ else:
   yeartrue = True
 if authfirsttrue == False or authlasttrue == False or webtitletrue == False or urltrue == False or monthtrue == False or daytrue == False or yeartrue == False:
   print ("At least 1 error has been found in values given. Scanning for errors in dates...")
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        pass
- 
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
-        return True
-    except (TypeError, ValueError):
-        pass
- 
-    return False
+  retry = True
+if sponser == "":
+  if sponser == "" and article == "":
+    print (authlast + ',' + authfirst + '. "' + webtitle + ', ' + daypub + ' ' + monthpub + '. ' + yearpub + ', ' + url + '.')
+  else:
+    print (authlast + ',' + authfirst + '. "' + article + '." ' + webtitle + ', ' + daypub + ' ' + monthpub + '. ' + yearpub + ', ' + url + '.')
+elif article == "":
+  print (authlast + ',' + authfirst + '." ' + webtitle + ', ' + sponser + ', ' + daypub + ' ' + monthpub + '. ' + yearpub + ', ' + url + '.')
+else:
+  print (authlast + ',' + authfirst + '. "' + article + '." ' + webtitle + ', ' + sponser + ', ' + daypub + ' ' + monthpub + '. ' + yearpub + ', ' + url + '.')
+print ("\nBibliography generated. Please restart the program to make another one.")
+
+       
 
 
